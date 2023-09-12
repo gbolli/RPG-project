ea8c6bd1c906e56626913ae67d4f56a68ad76e06
Gord Bolli <gord@bolli.ca>
2023-09-11 18:36:28 -0700

    Cast ray from camera and Debug.DrawRay.  Change name of scene to sandbox.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,8 +6,18 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
 
+    Ray lastRay;
+
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.Log("mouse button 0 down");
+        }
+
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.white);
+
         GetComponent<UnityEngine.AI.NavMeshAgent>().destination = target.position;
     }
 }
