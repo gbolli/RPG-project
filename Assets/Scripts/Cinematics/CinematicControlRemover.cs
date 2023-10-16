85cc8410abfca2a2fbae599fe888b44040a73279
Gord Bolli <gord@bolli.ca>
2023-10-16 14:38:10 -0700

    Complete DisableControl and EnableControl in cinematicControlRemover

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -2,25 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 namespace RPG.Cinematics
 {
     public class CinematicControlRemover : MonoBehaviour
     {
+
+        GameObject player;
         private void Start()
         {
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
+            player = GameObject.FindWithTag("Player");
         }
 
         void DisableControl(PlayableDirector director)
         {
-            print("disablecontrol");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector director)
         {
-            print("enablecontrol");
+            player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
