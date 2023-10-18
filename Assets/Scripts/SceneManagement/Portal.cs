1c3d481f2272c424a8837d131bb8dafcf4cffb44
Gord Bolli <gord@bolli.ca>
2023-10-17 18:06:27 -0700

    Set up Coroutine for scene management

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -13,8 +13,18 @@ namespace RPG.SceneManagement
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);  // prevent destroy when new scene is loaded
+            // Portals need to be at scene root level for DontDestroyOnLoad
+            
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);  // LoadSceneAsync return object that unity will use
+            print("Scene Loaded");
+            Destroy(gameObject);  // destroy to clean up after code is run
         }
     }
 }
