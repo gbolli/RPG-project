75e71163e5f959a3dfa35448b23bc75961a57a9a
Gord Bolli <gord@bolli.ca>
2023-10-18 14:15:20 -0700

    Create spawnPoint within portal and remember portal for return trip

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -23,8 +25,28 @@ namespace RPG.SceneManagement
             // Portals need to be at scene root level for DontDestroyOnLoad
             
             yield return SceneManager.LoadSceneAsync(sceneToLoad);  // LoadSceneAsync return object that unity will use
-            print("Scene Loaded");
+
+            Portal otherPortal = GetTheOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);  // destroy to clean up after code is run
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.transform.position;
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+        }
+
+        private Portal GetTheOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;  // we don't want this portal
+                return portal;
+            }
+            return null;  // can't find portal
         }
     }
 }
