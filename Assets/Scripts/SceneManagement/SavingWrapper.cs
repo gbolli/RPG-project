37ab312e660fb3d53081e9bbe755a931f3ccc9e6
Gord Bolli <gord@bolli.ca>
2023-10-29 22:15:43 -0700

    Create Saving object in PersistantObjects prefab.   Create SavingWrapper.cs

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Saving;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.L)) {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S)) {
+                Save();
+            }
+        }
+
+        private void Load()
+        {
+            GetComponent<JsonSavingSystem>().Load(defaultSaveFile);
+        }
+
+        private void Save()
+        {
+            GetComponent<JsonSavingSystem>().Save(defaultSaveFile);
+        }
+    }
+}
