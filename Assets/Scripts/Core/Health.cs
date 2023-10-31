b7c06de1e49a221af1140811d0d567c9fc84ff9a
Gord Bolli <gord@bolli.ca>
2023-10-30 23:54:46 -0700

    Implement IJsonSavable in Health script

--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, IJsonSaveable
     {
         [SerializeField] int health = 100;
 
         bool isDead = false;
+
         public bool IsDead()
         {
             return isDead;
@@ -17,13 +20,34 @@ namespace RPG.Core
         public void TakeDamage(int damage)  //  Take damage to health
         {
             health = Mathf.Max(0, health - damage);   // 0 is lowest health
+
             if (health <= 0)
             {
-                if (isDead) return;
-                isDead = true;
+                Die();
+            }
+        }
 
-                GetComponent<Animator>().SetTrigger("die");   // Trigger death animation
-                GetComponent<ActionScheduler>().CancelCurrentAction();  // Cancel from action scheduler
+        private void Die()
+        {
+            if (isDead) return;
+            isDead = true;
+
+            GetComponent<Animator>().SetTrigger("die");   // Trigger death animation
+            GetComponent<ActionScheduler>().CancelCurrentAction();  // Cancel from action scheduler
+        }
+
+        public JToken CaptureAsJToken()
+        {
+            return JToken.FromObject(health);
+        }
+
+        public void RestoreFromJToken(JToken state)
+        {
+            health = state.ToObject<int>();
+
+            if (health <= 0)
+            {
+                Die();
             }
         }
     }
