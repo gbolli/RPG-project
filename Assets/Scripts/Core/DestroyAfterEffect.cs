9907de5f1bcad4f0b14a85f1b6b471dee7b69222
Gord Bolli <gord@bolli.ca>
2024-02-05 09:21:06 -0800

    Create DestroyAfterEffect script to remove particle systems after completion.   Apply to Effects (Fireball, Generic)

---
+++ Assets/Scripts/Core/DestroyAfterEffect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterEffect : MonoBehaviour
+{
+    void Update()
+    {
+        if (!GetComponent<ParticleSystem>().IsAlive()) Destroy(gameObject);
+    }
+}
