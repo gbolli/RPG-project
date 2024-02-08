9f1d42625f566ce30839fc475c0f920c188ce6c3
Gord Bolli <gord@bolli.ca>
2024-02-08 08:36:54 -0800

    Weapon Pickup:  Hide and respawn after seconds instead of destroy.

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
@@ -7,12 +8,31 @@ namespace RPG.Combat {
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 5f;
 
         private void OnTriggerEnter(Collider other) {
             if (other.gameObject.tag == "Player") {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
         }
+
+        private IEnumerator HideForSeconds(float respawnTime)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(respawnTime);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+
+            // loop through children
+            foreach (Transform child in transform) {
+                child.gameObject.SetActive(shouldShow);
+            }
+        }
+
     }
 }
