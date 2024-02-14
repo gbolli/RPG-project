4f5d3327a0ac157df4a995fffda4ae43d916eeb8
Gord Bolli <gord@bolli.ca>
2024-02-13 17:35:59 -0800

    Add saving for experience points.

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Attributes {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, IJsonSaveable
     {
         [SerializeField] int experiencePoints = 0;
+
 
         public void GainExperience(int exp) {
             experiencePoints += exp;
 
             // possibly check for gain level here?
+        }
+
+        public JToken CaptureAsJToken()
+        {
+            return JToken.FromObject(experiencePoints);
+        }
+
+        public void RestoreFromJToken(JToken state)
+        {
+            experiencePoints = state.ToObject<int>();
         }
     }
 }
