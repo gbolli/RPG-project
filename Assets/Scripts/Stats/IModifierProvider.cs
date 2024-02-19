cde7b60bbf91ab4ad372acc4a18d0617fa3bea32
Gord Bolli <gord@bolli.ca>
2024-02-19 10:22:34 -0800

    Create interface IModifierProvider to get modifiers to add to base stats

---
+++ Assets/Scripts/Stats/IModifierProvider.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RPG.Stats {
+    public interface IModifierProvider {
+        IEnumerable<int> GetAdditiveModifiers(Stat stat);
+    }
+}
