ee4b6f9e4fc37a1f86a9587973ed37d96796682d
Gord Bolli <gord@bolli.ca>
2024-02-25 12:22:43 -0800

    Add GetCursorType to IRacastable and move enum to separate public script

--- Assets/Scripts/Control/IRaycastable.cs
+++ Assets/Scripts/Control/IRaycastable.cs
@@ -1,5 +1,6 @@
 namespace RPG.Control {
     public interface IRaycastable {
+        CursorType GetCursorType();
         bool HandleRaycast(PlayerController callingController);
     }
 }
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -14,13 +14,6 @@ namespace RPG.Control
     {
         Health health;
 
-        enum CursorType {
-            None,
-            Movement,
-            Combat,
-            UI
-        }
-
         [System.Serializable]
         struct CursorMapping {
             public CursorType type;
@@ -66,7 +59,7 @@ namespace RPG.Control
                 foreach (IRaycastable raycastable in raycastables)
                 {
                     if (raycastable.HandleRaycast(this)) {
-                        SetCursor(CursorType.Combat);
+                        SetCursor(raycastable.GetCursorType());
                         return true;
                     }
                 }
