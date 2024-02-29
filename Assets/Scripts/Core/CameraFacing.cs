0f69f608236d585d3ada2f1579f75ef0b958538b
Gord Bolli <gord@bolli.ca>
2024-02-29 09:05:09 -0800

    Create Damage text to appear above player being damaged.   Prefab and add script for CameraFacing

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace RPG.Core {
+    public class CameraFacing : MonoBehaviour
+    {     
+        void Update()
+        {
+            // which is better?   transform.rotation or transform.forward
+            transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
