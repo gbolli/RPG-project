c6b20b5855fd97d06a313b9e918efbe79a7137ee
Gord Bolli <gord@bolli.ca>
2024-03-06 10:09:35 -0800

    Move CameraFacing code from Update to LateUpdate.   Fix lag bug

--- Assets/Scripts/Core/CameraFacing.cs
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace RPG.Core {
     public class CameraFacing : MonoBehaviour
     {     
-        void Update()
+        void LateUpdate()
         {
             // which is better?   transform.rotation or transform.forward
             transform.rotation = Camera.main.transform.rotation;
